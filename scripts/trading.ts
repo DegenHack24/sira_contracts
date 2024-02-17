473cf22785824f018eabb454a0a16c5e72afcd6d
Bartosz Solka <bartosz.solka@gmail.com>
2024-02-17 13:55:44 +0100

    removed commented code

--- scripts/trading.ts
+++ scripts/trading.ts
@@ -24,42 +24,6 @@ async function main() {
     const equityToken = await ethers.getContractAt("EquityToken", equityTokenAddress, adminWallet);
     const tok = await ethers.getContractAt("TOK", tokAddress, adminWallet);
 
-    // console.log("Adding to allow list...");
-    // const allowListTx1 = await allowedList.addToAllowedList(wallet1.address);
-    // const allowListTx2 = await allowedList.addToAllowedList(wallet2.address);
-    // const allowListTx3 = await allowedList.addToAllowedList(tokAddress);
-
-    // await allowListTx1.wait();
-    // await allowListTx2.wait();
-    // await allowListTx3.wait();
-
-    // console.log("Adding to allow list done");
-
-    // console.log("Distributing tokens...");
-    // const equityTokenTx1 = await equityToken.creatItem(wallet1.address, 100);
-    // const equityTokenTx2 = await equityToken.creatItem(wallet2.address, 100);
-    // const stableCoinTx1 = await stableCoin.transfer(wallet1.address, 1000);
-    // const stableCoinTx2 = await stableCoin.transfer(wallet2.address, 1000);
-
-    // await equityTokenTx1.wait();
-    // await equityTokenTx2.wait();
-    // await stableCoinTx1.wait();
-    // await stableCoinTx2.wait();
-    // console.log("Distributing tokens done");
-
-    // console.log("Approving tokens...");
-    // const stableCoinApproveTx1 = await stableCoin.connect(wallet1).approve(tok.address, 1000);
-    // const stableCoinApproveTx2 = await stableCoin.connect(wallet2).approve(tok.address, 1000);
-    // const equityTokenApproveTx1 = await equityToken.connect(wallet1).approve(tok.address, 100);
-    // const equityTokenApproveTx2 = await equityToken.connect(wallet2).approve(tok.address, 100);
-
-    // await stableCoinApproveTx1.wait();
-    // await stableCoinApproveTx2.wait();
-    // await equityTokenApproveTx1.wait();
-    // await equityTokenApproveTx2.wait();
-    // console.log("Approving tokens done");
-
-
     console.log("Creating order...")
     const stableCoinPrice = 10;
     const equityTokenAmount = 5;
